9d5077001fba0827e6e102bc582f6809d1e76e28
“ZHtwinkle” <zhtwinkle@outlook.com>
2025-09-05 14:09:01 +0800

    修改名称，但保留作者 Github 地址

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,14 +41,14 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "zh_CN",
-    url: "https://moemail.app",
-    title: "MoeMail - 萌萌哒临时邮箱服务",
+    url: "https://ailab.email",
+    title: "AILab-Email - 临时邮箱服务",
     description: "安全、快速、一次性的临时邮箱地址，保护您的隐私，远离垃圾邮件。支持即时收件，到期自动失效。",
-    siteName: "MoeMail",
+    siteName: "AILab-Mail",
   },
   twitter: {
     card: "summary_large_image",
-    title: "MoeMail - 萌萌哒临时邮箱服务",
+    title: "AILab-Mail - 临时邮箱服务",
     description: "安全、快速、一次性的临时邮箱地址，保护您的隐私，远离垃圾邮件。支持即时收件，到期自动失效。",
   },
   manifest: '/manifest.json',
